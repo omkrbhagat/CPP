0bb951e31150b79a89436c9094b0b1afaa2076d9
omkrbhagat <onkarabhagat@outlook.com>
2026-09-14 16:56:26 +0530

    Added a file

---
+++ app.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4};
+    cout << vec.size() << endl;
+    // cout << "Hello, World !";
+}
